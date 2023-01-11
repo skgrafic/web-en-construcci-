e0ddb0db08cae2868bb06353eeac54d99b4bb351
Jose Carlos Gil <josec.gilma@gmail.com>
2023-01-11 17:00:02 +0100

    imatge index

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-  
+
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-img
+        max-height="100vh"
+        max-width="100vw"
+        src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        >
+        <v-row>
+        <v-col cols="12" md="4">
+          
+        </v-col>
+        <v-col cols="12" md="4" align="center" class="imatge">
+          <h2>Estem viatjant per tot el món preparant la nostra web perquè tinguis la millor experiència en el teu pròxim viatge.</h2>
+        </v-col>
+        <v-col cols="12" md="4"></v-col>
+        </v-row>
+      
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+
 </template>
+
+<style>
+
+.imatge{
+  margin-top:40vh;
+  color:white;
+  text-shadow: black 0.1em 0.1em 0.2em;
+}
+
+</style>
